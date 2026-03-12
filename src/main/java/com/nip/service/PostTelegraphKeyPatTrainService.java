9b0e0e5d57b91bb697a9223b0a97f5e5bc2e6939
Jungle <39988332+JungleZy@users.noreply.github.com>
2026-03-12 12:40:21 +0800

    fix: 修复随机报生成逻辑

--- src/main/java/com/nip/service/PostTelegraphKeyPatTrainService.java
+++ src/main/java/com/nip/service/PostTelegraphKeyPatTrainService.java
@@ -47,12 +47,12 @@ public class PostTelegraphKeyPatTrainService {
 
   @Inject
   public PostTelegraphKeyPatTrainService(UserService userService,
-                                         PostTelegraphKeyPatTrainDao patTrainDao,
-                                         GradingRuleDao gradingRuleDao,
-                                         PostTelegraphKeyPatTrainPageDao pageDao,
-                                         PostTelegraphKeyPatTrainPageValueDao valueDao,
-                                         PostTelegraphKeyPatTrainMoreEntityDao moreEntityDao,
-                                         CableFloorService cableFloorService) {
+      PostTelegraphKeyPatTrainDao patTrainDao,
+      GradingRuleDao gradingRuleDao,
+      PostTelegraphKeyPatTrainPageDao pageDao,
+      PostTelegraphKeyPatTrainPageValueDao valueDao,
+      PostTelegraphKeyPatTrainMoreEntityDao moreEntityDao,
+      CableFloorService cableFloorService) {
     this.userService = userService;
     this.patTrainDao = patTrainDao;
     this.gradingRuleDao = gradingRuleDao;
@@ -61,7 +61,6 @@ public class PostTelegraphKeyPatTrainService {
     this.moreEntityDao = moreEntityDao;
     this.cableFloorService = cableFloorService;
   }
-
 
   @Transactional
   public PostTelegraphKeyPatTrainVO add(PostTelegraphKeyPatTrainDto dto, String token) {
@@ -73,7 +72,7 @@ public class PostTelegraphKeyPatTrainService {
         .setErrorNumber(0)
         .setStatus(NOT_STARTED.getStatus());
 
-    //根据id查询评分规则
+    // 根据id查询评分规则
     GradingRuleEntity ruleEntity = Optional.ofNullable(gradingRuleDao.findById(entity.getRuleId()))
         .orElseThrow(() -> new IllegalArgumentException("未查询到该规则"));
     entity.setScore(new BigDecimal(ruleEntity.getScore()));
@@ -86,7 +85,8 @@ public class PostTelegraphKeyPatTrainService {
       }
       generatePatKey(totalNumber, 1, save.getId(), entity.getMessageType());
     } else {
-      List<List<List<String>>> cableFloor = cableFloorService.findCableFloor(dto.getCableId(), null, dto.getStartPage());
+      List<List<List<String>>> cableFloor = cableFloorService.findCableFloor(dto.getCableId(), null,
+          dto.getStartPage());
       int totalPage = dto.getTotalNumber() / 100;
       cableFloor = cableFloor.subList(0, totalPage);
       // 使用批量保存替代循环逐条保存，提升性能
@@ -109,17 +109,14 @@ public class PostTelegraphKeyPatTrainService {
     return PojoUtils.convertOne(save, PostTelegraphKeyPatTrainVO.class);
   }
 
-
   public List<PostTelegraphKeyPatTrainVO> listPage(String token) {
     UserEntity userEntity = userService.getUserByToken(token);
     List<PostTelegraphKeyPatTrainEntity> entityList = patTrainDao.find("createUserId = ?1",
         Sort.by("createTime").descending(),
-        userEntity.getId()
-    ).list();
+        userEntity.getId()).list();
 
     return PojoUtils.convert(entityList, PostTelegraphKeyPatTrainVO.class);
   }
-
 
   @Transactional
   public void begin(PostTelegraphKeyPatTrainDto dto) {
@@ -130,13 +127,12 @@ public class PostTelegraphKeyPatTrainService {
     patTrainDao.save(entity);
   }
 
-
   @Transactional
   public PostTelegraphKeyPatTrainVO finish(PostTelegraphKeyPatTrainDto dto) {
     try {
       PostTelegraphKeyPatTrainEntity entity = Optional.ofNullable(patTrainDao.findById(dto.getId()))
           .orElseThrow(() -> new IllegalArgumentException(TRAINING_NOT_FOUND));
-      //分数
+      // 分数
       PostTelegraphKeyPatTrainEntity save = countScore(entity, dto);
       return PojoUtils.convertOne(save, PostTelegraphKeyPatTrainVO.class);
     } catch (Exception e) {
@@ -151,12 +147,13 @@ public class PostTelegraphKeyPatTrainService {
       PostTelegraphKeyPatTrainEntity entity = Optional.ofNullable(patTrainDao.findById(id))
           .orElseThrow(() -> new IllegalArgumentException(TRAINING_NOT_FOUND));
       List<Integer> pageNumber = pageDao.countPageNumber(id);
-      //查询前2页数据content
+      // 查询前2页数据content
       List<PostTelegraphKeyPatTrainPageEntity> twoPage = pageDao.findTwoPage(id);
       List<PostTelegraphKeyPatTrainPageValueEntity> twoPageValue = valueDao.findTwoPage(id);
-      //统计每页拍发时长和个数
-      List<PostTelegraphKeyPatTrainPageValueEntity> pageValueEntities = valueDao.findByTrainIdOrderByPageNumberAscSortAsc(
-          id);
+      // 统计每页拍发时长和个数
+      List<PostTelegraphKeyPatTrainPageValueEntity> pageValueEntities = valueDao
+          .findByTrainIdOrderByPageNumberAscSortAsc(
+              id);
       Map<Integer, List<PostTelegraphKeyPatTrainPageValueEntity>> collect = pageValueEntities.stream().collect(
           Collectors.groupingBy(PostTelegraphKeyPatTrainPageValueEntity::getPageNumber));
       List<PostTelegraphKeyPatTrainPageAnalyzeVO> analyzeVOS = new ArrayList<>();
@@ -191,14 +188,15 @@ public class PostTelegraphKeyPatTrainService {
         }
         v.setExistPage(pageNumber);
         if (v.getStatus().compareTo(2) == 0) {
-          //在大于2页报文时，用户拍发的页数少于生成页数，需用生成的报文补足2数据
+          // 在大于2页报文时，用户拍发的页数少于生成页数，需用生成的报文补足2数据
           if (twoPageValue.size() < twoPage.size()) {
             int index = twoPage.size() - twoPageValue.size();
             for (int i = 0; i < index; i++) {
               try {
-                if (index+i<twoPage.size()){
+                if (index + i < twoPage.size()) {
                   if (null != twoPage.get(index + i)) {
-                    twoPageValue.add(PojoUtils.convertOne(twoPage.get(index + i), PostTelegraphKeyPatTrainPageValueEntity.class));
+                    twoPageValue.add(
+                        PojoUtils.convertOne(twoPage.get(index + i), PostTelegraphKeyPatTrainPageValueEntity.class));
                   }
                 }
               } catch (Exception e) {
@@ -222,13 +220,12 @@ public class PostTelegraphKeyPatTrainService {
     }
   }
 
-
   public PostTelegraphKeyPatTrainPageVO getPage(String trainId, Integer pageNumber) {
     PostTelegraphKeyPatTrainPageVO ret = new PostTelegraphKeyPatTrainPageVO();
     PostTelegraphKeyPatTrainEntity entity = Optional.ofNullable(patTrainDao.findById(trainId))
         .orElseThrow(() -> new IllegalArgumentException("未查询到训练id"));
     List<PostTelegraphKeyPatTrainPageEntity> messageVO;
-    //页码是否正确
+    // 页码是否正确
     int totalPage;
     int totalNumber;
     int generateNumber = 100;
@@ -246,34 +243,32 @@ public class PostTelegraphKeyPatTrainService {
       }
     }
 
-    //用户拍发内容
+    // 用户拍发内容
     List<PostTelegraphKeyPatTrainPageValueEntity> userPage = valueDao.find("trainId = ?1 and pageNumber = ?2",
         Sort.by("sort").ascending(), trainId,
-        pageNumber
-    ).list();
+        pageNumber).list();
 
-    //生成的内容
+    // 生成的内容
     List<PostTelegraphKeyPatTrainPageEntity> pageDaoAll = pageDao.find("trainId =?1 and pageNumber=?2 ",
-        Sort.by("sort").ascending(), trainId, pageNumber
-    ).list();
+        Sort.by("sort").ascending(), trainId, pageNumber).list();
     if (!pageDaoAll.isEmpty()) {
       messageVO = pageDaoAll;
     } else {
       messageVO = generatePatKey(generateNumber, pageNumber, entity.getId(), entity.getMessageType());
     }
-    //用户未拍发本页内容，则获取生成的内容
+    // 用户未拍发本页内容，则获取生成的内容
     if (userPage.isEmpty()) {
       ret.setMessageVO(PojoUtils.convert(messageVO, PostTelegraphKeyPatTrainPageMessageVO.class));
     } else {
       ret.setMessageVO(PojoUtils.convert(userPage, PostTelegraphKeyPatTrainPageMessageVO.class));
     }
 
-    //获取解析后的内容
+    // 获取解析后的内容
     List<String> resolver = pageDaoAll.stream().map(PostTelegraphKeyPatTrainPageEntity::getValue)
         .toList();
     ret.setResolverMessage(resolver);
 
-    //获取到本页的多组多行信息
+    // 获取到本页的多组多行信息
     PostTelegraphKeyPatTrainMoreEntity trainMoreEntity = moreEntityDao.findByTrainIdAndPageNumber(trainId, pageNumber);
     if (!Objects.isNull(trainMoreEntity)) {
       String moreLine = trainMoreEntity.getMoreLine();
@@ -307,7 +302,8 @@ public class PostTelegraphKeyPatTrainService {
       valueDao.deleteByTrainIdAndPageNumber(trainId, pageNumber);
       List<PostTelegraphKeyPatTrainPageValueEntity> list = new ArrayList<>();
       for (PostTelegraphKeyPatTrainPageMessageVO postTelegraphKeyPatTrainPageMessageVO : vo) {
-        PostTelegraphKeyPatTrainPageValueEntity postTelegraphKeyPatTrainPageValueEntity = PojoUtils.convertOne(postTelegraphKeyPatTrainPageMessageVO, PostTelegraphKeyPatTrainPageValueEntity.class);
+        PostTelegraphKeyPatTrainPageValueEntity postTelegraphKeyPatTrainPageValueEntity = PojoUtils
+            .convertOne(postTelegraphKeyPatTrainPageMessageVO, PostTelegraphKeyPatTrainPageValueEntity.class);
         postTelegraphKeyPatTrainPageValueEntity.setId(null);
         list.add(postTelegraphKeyPatTrainPageValueEntity);
       }
@@ -324,49 +320,51 @@ public class PostTelegraphKeyPatTrainService {
    * @param: entity
    */
   private PostTelegraphKeyPatTrainEntity countScore(PostTelegraphKeyPatTrainEntity entity,
-                                                    PostTelegraphKeyPatTrainDto dto) {
+      PostTelegraphKeyPatTrainDto dto) {
     entity.setStatus(PostTelegraphKeyPatTrainEnum.FINISH.getStatus());
     entity.setEndTime(LocalDateTime.now());
     entity.setContent(dto.getContent());
 
-    //计算训练时长
+    // 计算训练时长
     long time = entity.getEndTime().toEpochSecond(ZoneOffset.of("+8")) - entity.getBeginTime()
         .toEpochSecond(ZoneOffset.of("+8"));
     entity.setDuration(String.valueOf(time));
 
-    //查询扣分规则
+    // 查询扣分规则
     String ruleContent = Optional.ofNullable(gradingRuleDao.findById(entity.getRuleId()))
         .map(GradingRuleEntity::getContent).orElse("");
     PostKeyPatTrainRuleDto rule = JSONUtils.fromJson(ruleContent, PostKeyPatTrainRuleDto.class);
     entity.setRuleContent(ruleContent);
 
-    //积分规则
+    // 积分规则
     KeyPatStatisticalDto ks = new KeyPatStatisticalDto();
-    //得到已存在的页
+    // 得到已存在的页
     List<Integer> pageNumbers = valueDao.findAllByTrainIdToPageNumber(entity.getId());
     // 创建每页的处理结果，该结果会在每页处理完毕后替换旧的page_value数据
     List<KeyPatValueTransferDto> pageValueResult = new ArrayList<>();
     // 并行处理每页数据
     pageNumbers.parallelStream().forEach(pageNumber -> {
       // 根据page获取目标数据
-      List<KeyPatPageTransferDto> userPages = PojoUtils.convert(pageDao.findByTrainIdAndPageNumberOrderBySort(entity.getId(), pageNumber), KeyPatPageTransferDto.class);
+      List<KeyPatPageTransferDto> userPages = PojoUtils.convert(
+          pageDao.findByTrainIdAndPageNumberOrderBySort(entity.getId(), pageNumber), KeyPatPageTransferDto.class);
       // 根据page获取拍发数据
-      List<KeyPatValueTransferDto> userPageValues = PojoUtils.convert(valueDao.findByTrainIdAndPageNumberOrderBySort(entity.getId(), pageNumber), KeyPatValueTransferDto.class);
+      List<KeyPatValueTransferDto> userPageValues = PojoUtils.convert(
+          valueDao.findByTrainIdAndPageNumberOrderBySort(entity.getId(), pageNumber), KeyPatValueTransferDto.class);
       List<KeyPatValueTransferDto> pageResult = new ArrayList<>();
       handle(null, pageResult, userPages, userPageValues, ks);
       pageValueResult.addAll(pageResult);
     });
 
-    List<PostTelegraphKeyPatTrainPageValueEntity> pv = PojoUtils.convert(pageValueResult, PostTelegraphKeyPatTrainPageValueEntity.class);
+    List<PostTelegraphKeyPatTrainPageValueEntity> pv = PojoUtils.convert(pageValueResult,
+        PostTelegraphKeyPatTrainPageValueEntity.class);
     valueDao.deleteByTrainId(entity.getId());
     valueDao.saveAndFlush(pv);
 
-    //存放扣分规则 key扣分名称，value扣分值
+    // 存放扣分规则 key扣分名称，value扣分值
     Map<String, Object> deductInfo = new HashMap<>();
-    //计算少行
-    int totalGroups = entity.getIsCable() == 1 ?
-        (int) pageDao.count("trainId", entity.getId()) :
-        entity.getTotalNumber();
+    // 计算少行
+    int totalGroups = entity.getIsCable() == 1 ? (int) pageDao.count("trainId", entity.getId())
+        : entity.getTotalNumber();
     int fullPages = totalGroups / 100;
     int lastPageGroups = totalGroups % 100;
     int totalPages = fullPages + (lastPageGroups > 0 ? 1 : 0);
@@ -381,27 +379,28 @@ public class PostTelegraphKeyPatTrainService {
       ks.setLackLine(ks.getLackLine() + missingPages * 10);
     }
 
-    //计算速率 拍发个数/训练时长*60
+    // 计算速率 拍发个数/训练时长*60
     BigDecimal speed = new BigDecimal("0");
     if (ks.getPat() != 0) {
       speed = new BigDecimal(ks.getPat()).divide(new BigDecimal(4), 10, RoundingMode.HALF_UP)
-          .divide(new BigDecimal(ks.getPatTime()).divide(new BigDecimal(1000), 10, RoundingMode.HALF_UP), 10, RoundingMode.HALF_UP)
+          .divide(new BigDecimal(ks.getPatTime()).divide(new BigDecimal(1000), 10, RoundingMode.HALF_UP), 10,
+              RoundingMode.HALF_UP)
           .multiply(new BigDecimal(60)).setScale(0, RoundingMode.HALF_UP);
     }
 
     entity.setSpeed(String.valueOf(speed));
 
-    //错误个数
+    // 错误个数
     entity.setErrorNumber(ks.getError());
     BigDecimal accuracy = new BigDecimal("0");
     if (ks.getPatGroup() - ks.getError() != 0) {
-      //计算正确率 （拍发总个数 - 错误个数- 多字- 少字)） /拍发总个数
+      // 计算正确率 （拍发总个数 - 错误个数- 多字- 少字)） /拍发总个数
       accuracy = new BigDecimal(ks.getPatGroup() - ks.getError() - ks.getBunchGroup()).divide(
           new BigDecimal(ks.getPatGroup()), 2, RoundingMode.HALF_UP).multiply(new BigDecimal(100));
     }
     entity.setAccuracy(accuracy.doubleValue());
 
-    //得到要扣的分
+    // 得到要扣的分
     String minus = "-";
     BigDecimal score = entity.getScore();
     BigDecimal errorScore = rule.getOther().getErrorCode().multiply(new BigDecimal(ks.getError()));
@@ -432,21 +431,20 @@ public class PostTelegraphKeyPatTrainService {
     deductInfo.put("moreGroupNumber", ks.getMoreGroup());
     deductInfo.put("moreGroupScore", minus + moreGroupScore);
 
-    //改错
+    // 改错
     BigDecimal alterScore = rule.getOther().getAlterError().multiply(new BigDecimal(ks.getAlterError()));
     deductInfo.put("alterErrorNumber", ks.getAlterError());
     deductInfo.put("alterErrorScore", minus + alterScore);
 
-    //串组
+    // 串组
     BigDecimal bunchGroupScore = rule.getOther().getBunchGroup().multiply(new BigDecimal(ks.getBunchGroup()));
     deductInfo.put("bunchGroupNumber", ks.getBunchGroup());
     deductInfo.put("bunchGroupScore", minus + bunchGroupScore);
 
-    //少间隔
+    // 少间隔
     BigDecimal lackGapScore = rule.getOther().getLessGap().multiply(new BigDecimal(ks.getLackGap()));
     deductInfo.put("lackGapNumber", ks.getLackGap());
     deductInfo.put("lackGapScore", minus + lackGapScore);
-
 
     score = score.subtract(errorScore)
         .subtract(lackScore)
@@ -459,7 +457,7 @@ public class PostTelegraphKeyPatTrainService {
         .subtract(bunchGroupScore)
         .subtract(lackGapScore);
 
-    //判断速率是加分还是扣分
+    // 判断速率是加分还是扣分
     deductInfo.put("speedNumber", speed.toString());
     if (speed.compareTo(new BigDecimal(rule.getWpm().getBase())) > 0) {
       int diff = speed.intValue() - rule.getWpm().getBase();
@@ -474,7 +472,7 @@ public class PostTelegraphKeyPatTrainService {
     }
 
     entity.setScore(score);
-    //保存扣分详情
+    // 保存扣分详情
     entity.setDeductInfo(JSONUtils.toJson(deductInfo));
 
     return patTrainDao.save(entity);
@@ -490,7 +488,7 @@ public class PostTelegraphKeyPatTrainService {
    */
   @Transactional
   public List<PostTelegraphKeyPatTrainPageEntity> generatePatKey(Integer generateNumber, Integer pageNumber,
-                                                                 String trainId, Integer messageType) {
+      String trainId, Integer messageType) {
     List<PostTelegraphKeyPatTrainPageEntity> pageEntities = new ArrayList<>();
     int totalPage = generateNumber / 100;
     if (generateNumber % 100 > 0) {
@@ -502,9 +500,9 @@ public class PostTelegraphKeyPatTrainService {
         generate = generateNumber - i * 100;
       }
       List<String> messages = switch (messageType) {
-        case 0 -> GlobalMessageGeneratedUtil.generatedNumber(generate, true, true);
-        case 1 -> GlobalMessageGeneratedUtil.generatedWord(generate, true, true);
-        default -> GlobalMessageGeneratedUtil.generatedMingle(generate, true, true);
+        case 0 -> GlobalMessageGeneratedUtil.generatedNumber(generate, false, true);
+        case 1 -> GlobalMessageGeneratedUtil.generatedWord(generate, false, true);
+        default -> GlobalMessageGeneratedUtil.generatedMingle(generate, false, true);
       };
       for (int j = 0; j < messages.size(); j++) {
         String message = messages.get(j);
